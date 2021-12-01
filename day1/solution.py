e31ecf3756744807bd4fa955a238cf3daa581cc9
Alessio <alex.robassomero@gmail.com>
2021-12-01 18:05:21 +0100

    Fix solution for day 1.

--- day1/solution.py
+++ day1/solution.py
@@ -1,10 +1,11 @@
-from utils.utils import read_from_input
+def read_from_input():
+    with open('input.txt', 'r') as f:
+        return f.readlines()
 
 lines = [int(line) for line in read_from_input()]
 
 number_of_increases = 0
-
-subgroups = [sum(lines[i:i+3]) for i in range(0, len(lines))]
+subgroups = [sum(lines[i:i+3]) for i in range(0, len(lines) - 2)]
 
 number_of_increases = sum([subgroups[i] < subgroups[i + 1] for i in range(0, len(subgroups) - 1)])
 
